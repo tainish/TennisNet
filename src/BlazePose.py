dea416d86be0ee0ee78af49d4cfe6bf03815bdb8
Taiga Nishida <tainish@nuevaschool.org>
2022-10-19 19:32:31 -0700

    Add video pose estimation

--- src/BlazePose.py
+++ src/BlazePose.py
@@ -4,61 +4,80 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-'''
-Annotates the pose of the player onto each frame of the video input.
+def video_pose(video_path):
+	'''
+	Annotates the pose of the player onto each frame of the video input.
 
-Args:
-    video file name: video file path is hardcoded to be ../test/input/ ; file extension must be cv2 supported
+	Args:
+		video file name: video file path is hardcoded to be ../test/input/ so only file name is necessary;
+						 file extension must be cv2 supported
 
-Return:
-    void, writes to output file in ../test/output/[file_name] with a copy of the original video with pose annotations on every frame
-'''
+	Return:
+		void, writes to output file in ../test/output/[file_name] with a copy of the original video with pose annotations on every frame
+	'''
 
-# For static images:
-IMAGE_FILES = []
-BG_COLOR = (192, 192, 192) # gray
-with mp_pose.Pose(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    min_detection_confidence=0.5) as pose:
+	BG_COLOR = (192, 192, 192) # gray
+	INPUT_PATH = '../test/input/'
+	OUTPUT_PATH = '../test/output/'
 
-    for idx, file in enumerate(IMAGE_FILES):
-        image = cv2.imread(file)
-        image_height, image_width, _ = image.shape
+	cap = cv2.VideoCapture(INPUT_PATH + video_path)
+	out = cv2.VideoWriter(OUTPUT_PATH + 'out_' + video_path, cv2.VideoWriter_fourcc('M','J','P','G'), 60, (cap.get(3), cap.get(4))) # 3 -> width, 4 -> height
 
-        # Convert the BGR image to RGB before processing.
-        results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+	with mp_pose.Pose(
+		static_image_mode=True,
+		model_complexity=2,
+		enable_segmentation=True,
+		min_detection_confidence=0.5) as pose:
 
-        if not results.pose_landmarks:
-            continue
+		while (cap.isOpened()):
+			ret, frame = cap.read()
 
-        '''
-        print(
-            f'Nose coordinates: ('
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-        )
-        '''
-        
-        annotated_image = image.copy()
+			# If video end, break
+			if not ret: break
 
-        # Draw segmentation on the image.
-        # To improve segmentation around boundaries, consider applying a joint
-        # bilateral filter to "results.segmentation_mask" with "image".
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        bg_image = np.zeros(image.shape, dtype=np.uint8)
-        bg_image[:] = BG_COLOR
-        annotated_image = np.where(condition, annotated_image, bg_image)
+			image_height, image_width, _ = frame.shape
 
-        # Draw pose landmarks on the image.
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+			# Convert the BGR image to RGB before processing.
+			results = pose.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-        # Plot pose world landmarks.
-        mp_drawing.plot_landmarks(
-            results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+			if not results.pose_landmarks:
+				continue
+
+			'''
+			print(
+				f'Nose coordinates: ('
+				f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+				f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+			)
+			'''
+
+			annotated_image = frame.copy()
+
+			# Draw segmentation on the frame.
+			# To improve segmentation around boundaries, consider applying a joint
+			# bilateral filter to "results.segmentation_mask" with "frame".
+			condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+			bg_image = np.zeros(frame.shape, dtype=np.uint8)
+			bg_image[:] = BG_COLOR
+			annotated_image = np.where(condition, annotated_image, bg_image)
+
+			# Draw pose landmarks on the frame.
+			mp_drawing.draw_landmarks(
+				annotated_image,
+				results.pose_landmarks,
+				mp_pose.POSE_CONNECTIONS,
+				landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+			
+			out.write(annotated_image)
+
+			'''
+			# Plot pose world landmarks.
+			mp_drawing.plot_landmarks(
+				results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+			'''
+	
+	# Release videos
+	cap.release(0)
+	out.release(0)
+
+	cv2.destoryAllWindows()
