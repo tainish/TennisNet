84daa4387fef2e02ecb95cd9cd60bc447ae2bfa8
agiatdonguri <taiganishida2009@gmail.com>
2022-10-20 04:18:16 -0700

    Added another test image

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from src.BlazePose import image_pose
+from util.BlazePose import image_pose
 
 if not image_pose('test\\'):
 	print("Not working")
 	exit()
 
-user_results = image_pose()
+user_results = image_pose(debug=True)
 pro_results = image_pose('ground_truth\\')
 
+
+print(len(user_results))
+print(len(pro_results))
