df44efd05d0600612c6735f7e8fe72c681bb850c
NUS16248-11-aletuch <aletuch@NUS16248-11-aletuchs-MacBook-Pro.local>
2022-11-14 20:34:42 -0800

    updating upload interface

--- upload.py
+++ upload.py
@@ -6,9 +6,15 @@ from video_process import split_frames
 def upload():
     sg.theme("GreenTan")
 
-    layout=[[[sg.Text("Upload a JPG Image: "), sg.FileBrowse(key="user_pose_img", file_types=(("JPG Images", "*.jpg"),))]], [sg.Button("Send Image")], [sg.Text("")], [sg.Text("OR:")], [sg.Text("")], [[sg.Text("Upload an AVI Video: "), sg.FileBrowse(key="user_pose_vid", file_types=(("AVI Video", "*.avi"),))]], [sg.Button("Send Video")]]
+    layout=[
+        [[sg.Text("Upload a JPG Image: "), sg.FileBrowse(key="user_pose_img", file_types=(("JPG Images", "*.jpg"),))], [sg.Button("Send Image")]],
+         [sg.Text("")], 
+         [sg.Text("OR:")], 
+         [sg.Text("")], 
+         [[sg.Text("Upload an AVI Video: "), sg.FileBrowse(key="user_pose_vid", file_types=(("AVI Video", "*.avi"),))], [sg.Button("Send Video")]]
+         ]
 
-    window = sg.Window(title="TennisNet - Pose Analysis", layout=layout, margins=(200, 200))
+    window = sg.Window(title="TennisNet - Pose Analysis", layout=layout, margins=(50, 50))
 
     while True: 
         event, values = window.read()
